c422828efcc89e7fef26ac0ffdc510cd56604a08
f7ord <geyserf7ord@gmail.com>
2025-04-10 20:35:41 +0000

    Added the main() function

--- hello.py
+++ hello.py
@@ -3,8 +3,11 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name',default='World', help='Name to greet')
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',default='World', help='Name to greet')
+    args = parser.parse_args()
+    print(f'Hello, {args.name}!')
 
-print(f'Hello, {args.name}!')
+if __name__ == '__main__':
+    main()
