f0b38e4e9ddc59fb4b2b3d88c30ea08e234b2584
f7ord <geyserf7ord@gmail.com>
2025-04-11 14:47:26 +0000

    Initial commit

---
+++ crowsnest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import argparse
+
+def get_args():
+    "Get the command line arguments"
+    parser = argparse.ArgumentParser(description="Crow's Nest Program")
+    parser.add_argument('word', type=str, help="The object to call out")
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    article = 'an' if args.word[0].lower() in 'aeiou' else 'a'
+    print(f'Ahoy, Captain, {article} {args.word} off the larboard bow!')
+
+
+if __name__ == '__main__':
+    main()
