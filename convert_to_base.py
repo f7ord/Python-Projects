1b948811ed826f61a72f1e3cb2fb044257782f46
f7ord <geyserf7ord@gmail.com>
2025-04-11 07:21:46 +0000

    Added a get_args() function to allow the program to take command-line arguments

--- convert_to_base.py
+++ convert_to_base.py
@@ -1,10 +1,20 @@
-#TODO: tweak the program so it takes command line arguments
+
 #TODO: make it work for higher bases like 16
+
+import argparse
+
+def get_args():
+    "Get the command-line arguments"
+    parser = argparse.ArgumentParser(description="Convert the given decimal number to base b")
+    parser.add_argument('num', help='the decimal number to convert', type=int)
+    parser.add_argument('-b', '--base', metavar='base', help='the base to convert the number to (default is 2)', default=2, type=int)
+    return parser.parse_args()
+
 
 def base_convert(num: int, b: int):
     """ Basic base conversion program
-        num: the number to convert
-        b: the base to convert the number to
+        num: the number to convert (decimal)
+        b: the base to convert the number to (default is 2)
         Return num in base b
     """
     ans = []
@@ -14,5 +24,10 @@ def base_convert(num: int, b: int):
     return ''.join(str(i) for i in ans)
 
 
-print(base_convert(int('1100', 2), 10))  # 12
-print(base_convert(256, 2))  # 100000000
+def main():
+    args = get_args()
+    print(base_convert(args.num, args.base))
+
+
+if __name__ == '__main__':
+    main()
