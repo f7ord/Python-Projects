d503f82496bd5072b97d0d81f7259fbdd1943369
f7ord <geyserf7ord@gmail.com>
2025-04-11 15:04:40 +0000

    Added a new parameter 'star' that, if present, changes the side to 'starboard'

--- crowsnest.py
+++ crowsnest.py
@@ -6,6 +6,7 @@ def get_args():
     "Get the command line arguments"
     parser = argparse.ArgumentParser(description="Crow's Nest Program")
     parser.add_argument('word', type=str, help="The object to call out")
+    parser.add_argument('-s', '--star', action='store_true', help="Changes larboard to starboard if present.")
     return parser.parse_args()
 
 
@@ -14,7 +15,8 @@ def main():
     word = args.word
     article = 'an' if args.word[0].lower() in 'aeiou' else 'a'
     article = article.title() if word[0].isupper() else article
-    print(f'Ahoy, Captain, {article} {args.word} off the larboard bow!')
+    side = 'starboard' if args.star else 'larboard'
+    print(f'Ahoy, Captain, {article} {args.word} off the {side} bow!')
 
 
 if __name__ == '__main__':
