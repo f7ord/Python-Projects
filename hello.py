57140b766d4d7fc70823778110617ff70a3575b9
f7ord <geyserf7ord@gmail.com>
2025-04-11 22:32:11 +0000

    Formatted the code

--- hello.py
+++ hello.py
@@ -3,16 +3,21 @@
 
 import argparse
 
+
 def get_args():
     """Get the arguments"""
-    parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name',default='World', help='Name to greet')
+    parser = argparse.ArgumentParser(
+        description="Say hello")
+    parser.add_argument(
+        "-n", "--name", metavar="name", default="World", help="Name to greet"
+    )
     return parser.parse_args()
+
 
 def main():
     args = get_args()
-    print(f'Hello, {args.name}!')
+    print(f"Hello, {args.name}!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
