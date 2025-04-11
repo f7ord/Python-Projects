990db608478191ba6147f9821a0ac4c18babf230
f7ord <geyserf7ord@gmail.com>
2025-04-11 16:54:39 +0000

    Added an option to allow users to specify the separator (default is ',')

--- picnic.py
+++ picnic.py
@@ -8,12 +8,14 @@ def get_args():
     parser.add_argument('items', metavar='items', nargs='+', help='Item(s) to bring')
     parser.add_argument('-s', '--sorted', action='store_true', help='Sort the items to bring, if present')
     parser.add_argument('-c', '--comma', action='store_true', help='If present, print without the Oxford comma.')
+    parser.add_argument('--sep', metavar='separator', default=',', help='The character to separate items.')
     return parser.parse_args()
 
 
 def main():
     args = get_args()
     items = args.items
+    sep = args.sep + ' '
     
     if args.sorted:
         items.sort()
@@ -24,9 +26,9 @@ def main():
         print(f'You are bringing {items[0]} and {items[1]}.')
     else:
         if args.comma:
-            print(f"You are bringing {', '.join(items[:-1])} and {items[-1]}.")
+            print(f"You are bringing {sep.join(items[:-1])} and {items[-1]}.")
         else:
-            print(f"You are bringing {', '.join(items[:-1])}, and {items[-1]}.")
+            print(f"You are bringing {sep.join(items[:-1])}{sep.rstrip()} and {items[-1]}.")
 
 
 if __name__ == '__main__':
