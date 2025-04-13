f849eaefdec13e6b23074aed41fdceb4f9fda92b
f7ord <geyserf7ord@gmail.com>
2025-04-13 13:39:31 +0000

    The program now allows users to specify the output file

--- howler/howler.py
+++ howler/howler.py
@@ -2,24 +2,30 @@
 
 import argparse
 import os
+import sys
+
 
 def get_args():
     """Get the command line arguments"""
     parser = argparse.ArgumentParser(description="Make the given text uppercase")
-    parser.add_argument('text', help="The message or file to transform")
-    return parser.parse_args()
+    parser.add_argument("text", help="The message or file to transform")
+    parser.add_argument("--out", "-o", default="", help="Output filename")
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+
+    return args
 
 
 def main():
     args = get_args()
-    text = args.text
-
-    if os.path.isfile(text):
-        content = open(text).read().rstrip()
-        print(content.upper())
-    else:
-        print(text.upper())
+    outfile = open(args.out, "wt") if args.out else sys.stdout
+    # outfile.write(args.text.upper() + "\n")
+    print(args.text.upper(), file=outfile)
+    outfile.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
