b3a8b902839bd4fbdd268316365b20173fa9efa6
f7ord <geyserf7ord@gmail.com>
2025-04-13 18:24:21 +0000

    Modified the program to handle multiple input files, and write each input file to the same filename in the output directory

--- howler/howler_v2.py
+++ howler/howler_v2.py
@@ -9,28 +9,41 @@ import io
 def get_args():
     """Get the command line arguments"""
     parser = argparse.ArgumentParser(description="Make the given text uppercase")
-    parser.add_argument("text", help="The message or file to transform")
-    parser.add_argument("--out", "-o", default="", help="Output filename")
-    parser.add_argument('-lc', action="store_true", help="The input will be printed in lowercase, if on")
+    parser.add_argument(
+        "text",
+        nargs="+",
+        help="The message or file to transform")
+    parser.add_argument(
+        "--outdir",
+        "-o", 
+        default="", 
+        help="Output filename")
+    parser.add_argument(
+        "-lc", 
+        action="store_true", 
+        help="The input will be printed in lowercase, if on"
+    )
 
     args = parser.parse_args()
 
-    if os.path.isfile(args.text):
-        args.text = open(args.text)
-    else:
-        args.text = io.StringIO(args.text + '\n')
-    
+    for i, f in enumerate(args.text):
+        if os.path.isfile(f):
+            args.text[i] = open(f)
+        else:
+            args.text[i] = io.StringIO(f + "\n")
+
     return args
 
 
 def main():
     args = get_args()
-    outfile = open(args.out, 'wt') if args.out else sys.stdout
+    outfile = open(args.outdir, "at") if args.outdir else sys.stdout
     out_case = str.lower if args.lc else str.upper
-    for line in args.text:
-        outfile.write(out_case(line))
+    for f in args.text:
+        for line in f:
+            outfile.write(out_case(line))
     outfile.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
