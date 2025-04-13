7f742dfaf0105a349c64106f9f6e4fe0c4bbaba2
f7ord <geyserf7ord@gmail.com>
2025-04-13 12:13:46 +0000

    The program can now read files provided by the user and print out their contents

--- howler/howler.py
+++ howler/howler.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
 
 def get_args():
     """Get the command line arguments"""
     parser = argparse.ArgumentParser(description="Make the given text uppercase")
-    parser.add_argument('text', help="The message to transform")
+    parser.add_argument('text', help="The message or file to transform")
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    print(args.text.upper())
+    text = args.text
+
+    if os.path.isfile(text):
+        content = open(text).read().rstrip()
+        print(content)
+    else:
+        print(text.upper())
+
 
 if __name__ == '__main__':
     main()
