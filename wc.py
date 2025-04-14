84c1b9d3d32c7f3855fe0c460945322d841edf02
f7ord <geyserf7ord@gmail.com>
2025-04-14 21:02:10 +0000

    The program now accepts flags to print the number of chars, the number of lines, and the number of words.

--- wc.py
+++ wc.py
@@ -20,12 +20,32 @@ def get_args():
         type=argparse.FileType('rt'),
         help="Input file(s)",
     )
+    parser.add_argument(
+        '--chars',
+        '-c',
+        action="store_true",
+        help="Print the number of characters"
+    )
+    parser.add_argument(
+        "--lines",
+        "-l",
+        action="store_true",
+        help="Print the number of lines"
+    )
+    parser.add_argument(
+        "--words",
+        "-w",
+        action="store_true",
+        help="Print the number of words"
+    )
+
     return parser.parse_args()
 
 
 def main():
     args = get_args()
     tnl, tnw, tnb = 0, 0, 0
+    c, l, w = args.chars, args.lines, args.words
 
     for fh in args.file:
         nl, nw, nb = 0, 0, 0
@@ -33,13 +53,36 @@ def main():
             nl += 1
             nw += len(line.split())
             nb += len(line)
-        print(f"{nl:8}{nw:8}{nb:8} {fh.name}")
         tnl += nl
         tnw += nw
         tnb += nb
-    
+
+        # printing the result
+        # print all the columns by default
+        if not any([l, w, c]):
+            print(f"{nl:8}{nw:8}{nb:8} {fh.name}")
+        else:
+            output = ""
+            total = ""
+            if l:
+                output += f"{nl:8}"
+                total += f"{tnl:8}"
+            if w:
+                output += f"{nw:8}"
+                total += f"{tnw:8}"
+            if c:
+                output += f"{nb:8}"
+                total += f"{tnb:8}"
+        
+            output += f" {fh.name}"
+            total += f" total"    
+            print(output)
+    # print the total if more than one file is passed    
     if len(args.file) > 1:
-        print(f"{tnl:8}{tnw:8}{tnb:8} total")
+        if not any([l,w,c]):
+            print(f"{tnl:8}{tnw:8}{tnb:8} total")
+        else:
+            print(total)
 
 
 if __name__ == '__main__':
