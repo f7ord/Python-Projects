078d4ca3887383cf05074a6d699b15b64b7c6ebe
f7ord <geyserf7ord@gmail.com>
2025-04-20 17:08:48 +0000

    Added a flag to collapse multiple adjacent vowels into a single substituted value

--- apple.py
+++ apple.py
@@ -22,6 +22,12 @@ def get_args():
         type=str,
         help="The vowel to substitute (default: a)"
     )
+    parser.add_argument(
+        "--collapse",
+        "-c",
+        action="store_true",
+        help="Collapse multiple adjacent vowels into a single substituted value. e.g., 'quick' becomes 'qack' and not 'qaack'"
+    )
 
     args = parser.parse_args()
     for i, f in enumerate(args.text):
@@ -35,12 +41,22 @@ def get_args():
 
 def main():
     args = get_args()
-    vowels = "aeiou"
+    vowels = "aeiouAEIOU"
+    vowel = args.vowel
 
     for f in args.text:
         for line in f:
-            line = line.translate(str.maketrans('aeiouAEIOU', args.vowel * 5 + args.vowel.upper() * 5))
-            print(line, end="")
+            if not args.collapse:
+                print(line.translate(str.maketrans('aeiouAEIOU', args.vowel * 5 + args.vowel.upper() * 5)), end="")
+            else:
+                result = line[0].translate(str.maketrans(vowels, args.vowel*5 + args.vowel.upper()*5))
+                for char in line[1:]:
+                    if result[-1] not in vowels and char in vowels:
+                        result += vowel
+                    elif char not in vowels:
+                        result += char
+                print(result, end="")
+                        
 
 
 if __name__ == "__main__":
