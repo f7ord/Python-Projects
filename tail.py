14ab1c0476c1a3951b374d344d4b4af38f685b25
f7ord <geyserf7ord@gmail.com>
2025-04-22 08:11:36 +0000

    The program can now output starting with line NUM

--- tail.py
+++ tail.py
@@ -22,8 +22,8 @@ def get_args():
         "-n",
         "--lines",
         default=10,
-        type=int,
-        help="Output the last n lines (default: 10)"
+        type=str,
+        help="Output the last n lines (default: 10); Use -n +NUM to output starting with line NUM"
     )
 
     return parser.parse_args()
@@ -32,12 +32,17 @@ def get_args():
 def main():
     args = get_args()
     files = args.file
-    n = args.lines
+    start_line = True if args.lines[0] == '+' else False
+    n = int(args.lines)
 
     for file in files:
         lines = file.readlines()
-        for line in lines[-n:]:
-            print(line.rstrip())
+        if start_line:
+            for line in lines[n-1:]:
+                print(line.rstrip())
+        else:
+            for line in lines[-n:]:
+                print(line.rstrip())
         file.close()
 
 
