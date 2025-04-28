6c262cbb87c1023536766b26f2af950469fd282b
f7ord <geyserf7ord@gmail.com>
2025-04-28 10:01:58 +0000

    Placed on_the_wall() outside main()

--- bottles.py
+++ bottles.py
@@ -11,39 +11,26 @@ def get_args():
         "--num",
         type=int,
         default=10,
-        help="Print all the verses from NUM down do 1"
+        help="Print all the verses from NUM down do 1 (default: 10)"
     )
 
+    args = parser.parse_args()
+    if args.num < 1:
+        parser.error(f"--num *{args.num}* must be greater than 0")
+
     return parser.parse_args()
+
+
+def on_the_wall(num, end=','):
+    bottle = 'bottles' if num > 1 else 'bottle'
+    print(f"{num} {bottle} of beer on the wall{end}")
+    if end != '!\n':
+        print(f"{num} {bottle} of beer,")
 
 
 def main():
     args = get_args()
     num = args.num
-
-    # def on_the_wall(num, end=','):
-    #     bottle = 'bottles' if num > 1 else 'bottle'
-    #     print(f"{num} {bottle} of beer on the wall{end}")
-    #     if end != '!':
-    #         print(f"{num} {bottle} of beer,")
-
-
-    # for _ in range(num):
-    #     on_the_wall(num)
-    #     print(f"Take one down, pass it around,")
-    #     if num != 1:
-    #         on_the_wall(num-1, end='!')
-    #         print("\n")
-    #         num -= 1
-    #     else:
-    #         print(f"No more bottles of beer on the wall!\n")
-
-    def on_the_wall(num, end=','):
-        bottle = 'bottles' if num > 1 else 'bottle'
-        print(f"{num} {bottle} of beer on the wall{end}")
-        if end != '!\n':
-            print(f"{num} {bottle} of beer,")
-
 
     for i in range(num, 0, -1):
         on_the_wall(i)
@@ -52,6 +39,7 @@ def main():
             on_the_wall(i-1, end='!\n')
         else:
             print(f"No more bottles of beer on the wall!")
+
 
 if __name__ == "__main__":
     main()
