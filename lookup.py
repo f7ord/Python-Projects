d736174c35170a373338c31c4c05d2e70ba71275
f7ord <geyserf7ord@gmail.com>
2025-05-07 21:12:44 +0000

    The program now prints a message to the user if no line starts with any of

--- lookup.py
+++ lookup.py
@@ -24,14 +24,27 @@ def get_args():
     return parser.parse_args()
 
 
+def format_letters_output(letters):
+    """Format"""
+    if len(letters) == 1:
+        return letters[0]
+    elif len(letters) == 2:
+        return ' and '.join(letters)
+    else:
+        return ', '.join(letters[:-1]) + f' and {letters[-1]}'
+
+
 def main():
     args = get_args()
     letters = tuple(x.lower() for x in args.letter)
-    file = args.file
-    
-    for line in file:
+    found = 0
+
+    for line in args.file:
         if line.lower().startswith(letters):
             print(line, end='')
+            found = 1
+    if not found:
+        print(f"Sorry we didn't find any lines that start with {format_letters_output(args.letter)}")
 
 
 if __name__ == "__main__":
