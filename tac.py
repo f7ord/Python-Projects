5ff03bb7e0ac883348c066c7f0a9429eae313dd9
f7ord <geyserf7ord@gmail.com>
2025-05-07 20:13:55 +0000

    Initial commit

---
+++ tac.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3 
+
+# print the lines of a file in reverse
+
+import argparse
+import sys
+
+
+def get_args():
+    """Get the command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description="Emulate tac: Write each FILE to STDOUT, last line first"
+    )
+    parser.add_argument(
+        'file',
+        metavar='FILE',
+        nargs='*',
+        default=[sys.stdin],
+        type=argparse.FileType('rt'),
+        help='Input file(s) (default: STDIN)'
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    for file in args.file:
+        lines = []
+        # capturing the lines
+        for line in file:
+            lines = [line.rstrip()] + lines
+        # printing the lines
+        print('\n'.join(lines)) 
+
+
+if __name__ == '__main__':
+    main()
