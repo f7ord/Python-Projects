9577aa85bed0cdfa80b3c0281fda97a4f0904e45
f7ord <geyserf7ord@gmail.com>
2025-06-08 23:57:06 +0000

    Initial commit: Find all the factors of the given number

---
+++ factors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Find all the factors of the given number
+
+import argparse
+
+
+def get_args():
+    """Get the number"""
+    parser = argparse.ArgumentParser(
+        description="Find all the factors"
+    )
+    parser.add_argument(
+        'num',
+        type=int,
+        help='The number, an integer'
+    )
+    return parser.parse_args()
+
+
+def factors(num: int):
+    """Find all the factors of num"""
+    ans = set()
+    for i in range(1, int(num**0.5)+1):
+        if not num % i:
+            ans.add(i)
+            ans.add(num // i)
+    return sorted(ans)
+
+
+def main():
+    print(factors(get_args().num))
+
+
+if __name__ == '__main__':
+    main()
